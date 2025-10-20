9460ed7fabb6b543fbb01683182e346b4921486f
JALAL <jalaluddin0046356@gmail.com>
2025-10-21 04:32:30 +0600

    Add .gitignore and clean repo

--- Tri-Gardening-Backend/src/uploads/uploads.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UploadsController } from './uploads.controller';
-import { AuthModule } from 'src/auth/auth.module';
-
-@Module({
-  imports: [AuthModule],
-  controllers: [UploadsController],
-})
-export class UploadsModule {}
